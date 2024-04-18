a648f76a32cdf843a71b0f75c1b0b5a64295d683
Johan Frisk <johan.frisk@opter.com>
2024-04-18 18:58:28 +0200

    Add project files.

---
+++ WpfApp8/DetailViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp8
+{
+    internal class DetailViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }        
+
+        private bool _option1;
+        public bool Option1
+        {
+            get { return _option1; }
+            set
+            {
+                _option1 = value;
+                OnPropertyChanged(nameof(Option1));
+            }
+        }
+    }
+}
